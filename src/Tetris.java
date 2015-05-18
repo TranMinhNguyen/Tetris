325eaf6a36786749a979938818f1487bbf24d0cf
HuyenX <HuyenX@192.168.64.118>
2015-05-18 16:59:34 +0700

    Tạo menu cho chương trình

---
+++ src/Tetris.java
@@ -0,0 +1,65 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+
+public class Tetris extends JFrame{
+
+	public Tetris(){
+		setSize(500, 600);
+		setTitle("Tetris");
+		setLayout(new BorderLayout());
+		setResizable(false);
+	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		CreateMenu();
+		
+		setVisible(true);
+	}
+	
+	public void CreateMenu(){
+		JMenuBar menubar = new JMenuBar();
+		JMenu menu;
+		JMenuItem menuitem;
+		
+		this.setJMenuBar(menubar);
+		menu=new JMenu("Game");
+		menu.setMnemonic(KeyEvent.VK_G);
+		menuitem = new JMenuItem("New Game");
+		menu.add(menuitem);
+		menuitem= new JMenuItem("Exit");
+		menuitem.setMnemonic(KeyEvent.VK_E);
+		menuitem.addActionListener(new MenuHandler(this));
+		menu.add(menuitem);
+		menubar.add(menu);
+	}
+	
+	public class MenuHandler implements ActionListener
+	   {
+		   Tetris ter;
+		   public MenuHandler(Tetris ter)
+		   {
+			   this.ter = ter;
+		   }
+		   public void actionPerformed(ActionEvent e) {
+			   String s = e.getActionCommand();
+			   if(s == "Exit")
+			   {
+				   ter.dispose();
+			   }
+		   }
+	   }
+	public static void main(String[] args) {
+		new Tetris(); 
+
+	}
+
+}
