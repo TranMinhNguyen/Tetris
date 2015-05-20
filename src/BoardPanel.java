092319f1e3fe537c33928feb9bcb5b983f4a0186
ADMIN <ADMIN@DELL>
2015-05-20 21:50:11 +0700

    hướng dẫn dùng Enter để bắt đầu chơi, hiển thị tên game TETRIS

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -24,6 +25,15 @@ public class BoardPanel extends JPanel{
 	public static final int COL_COUNT = 10; 
 	//so luong cot tren bang
 	
+	private static final int CENTER_X = COL_COUNT * TILE_SIZE / 2;
+	//toa do hien thi trung tam X
+	
+	 // font chu lon
+	private static final Font LARGE_FONT = new Font("Tahoma", Font.BOLD, 16);
+
+	 // font chu nho
+	private static final Font SMALL_FONT = new Font("Tahoma", Font.BOLD, 12);
+	
 	
 	public static final int PANEL_WIDTH = COL_COUNT * TILE_SIZE + BORDER_WIDTH * 2; 
 	//tong chieu rong cua bang dieu khien
@@ -44,6 +54,12 @@ public class BoardPanel extends JPanel{
 		
 		g.setColor(Color.WHITE);
 		g.drawRect(0, 0, TILE_SIZE * COL_COUNT, TILE_SIZE * VISIBLE_ROW_COUNT);
+		//"Press Enter to Play"
+		g.setFont(SMALL_FONT);
+		g.drawString("Press Enter to Play", CENTER_X - g.getFontMetrics().stringWidth("Press Enter to Play") / 2,300 );
+		//"TETRIS"
+		g.setFont(LARGE_FONT);
+		g.drawString("TETRIS", CENTER_X - g.getFontMetrics().stringWidth("TETRIS") / 2, 150);
 		 //ve duong ke cho bang
 		g.setColor(Color.DARK_GRAY);
 		for(int x = 0; x < COL_COUNT; x++) {
