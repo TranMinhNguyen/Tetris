d1a65e9a06eae4046a848f15fe74fa1a7bf2b6ab
Administrator <Administrator@OSHZJQYD6WD4ZI2>
2015-05-22 18:31:02 +0700

    Them nut pause P de dung lai,khi khoi gach roi xuong hang cuoi cung thi dung

--- src/Tetris.java
+++ src/Tetris.java
@@ -12,157 +12,166 @@ import javax.swing.JMenuItem;
 
 public class Tetris extends JFrame{
 	//time delay
-	private int sec;
-	
-	private int level;
-	private int score;
-	private int cotHienTai;
-	private int hangHienTai;
-	private boolean i=false;
-	
-	//huong quay hien tai
-	private int currentRotation;
-	private SidePanel sidepanel;
-	private BoardPanel boardpanel;
-	
-	
-	public Tetris(){
-		//setSize(500, 600);
-		setTitle("Tetris");
-		setLayout(new BorderLayout());
-		setResizable(false);
-	
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
-		
-		CreateMenu();
-		this.sidepanel=new SidePanel(this);
-		this.boardpanel=new BoardPanel(this);
-	
-		add(sidepanel,BorderLayout.EAST);
-		add(boardpanel,BorderLayout.CENTER);
-		
-		addKeyListener(new KeyAdapter() {
+			private int sec;
+			
+			private int level;
+			private int score;
+			private int cotHienTai;
+			private int hangHienTai;
+			private boolean isPaused=false;
+			
+			//huong quay hien tai
+			private int currentRotation;
+			private SidePanel sidepanel;
+			private BoardPanel boardpanel;
+			
+			
+			public Tetris(){
+				//setSize(500, 600);
+				setTitle("Tetris");
+				setLayout(new BorderLayout());
+				setResizable(false);
+			
+				setDefaultCloseOperation(EXIT_ON_CLOSE);
+				setLocationRelativeTo(null);
+				
+				CreateMenu();
+				this.sidepanel=new SidePanel(this);
+				this.boardpanel=new BoardPanel(this);
+			
+				add(sidepanel,BorderLayout.EAST);
+				add(boardpanel,BorderLayout.CENTER);
+				
+				addKeyListener(new KeyAdapter() {
 
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
+					@Override
+					public void keyPressed(KeyEvent e) {
+						// TODO Auto-generated method stub
+						
+						
+						switch(e.getKeyCode()){
+						
+						//nut Q dung de quay hinh sang trai
+						case KeyEvent.VK_Q:
+							if (!isPaused){
+								if (currentRotation==0) currentRotation=3;
+								else currentRotation--;
+								boardpanel.repaint();
+							break;}
+						//phim E dung de quay hinh sang phai	
+						case KeyEvent.VK_E:
+							if (!isPaused){
+								if (currentRotation==3) currentRotation=0;
+								else currentRotation++;
+								boardpanel.repaint();
+							break;}
+						//phim A dung de dich trai
+						case KeyEvent.VK_A:
+							if (!isPaused){
+								if (cotHienTai != 0) cotHienTai --;
+							break;}
+						//phim D dung de dich phai
+						case KeyEvent.VK_D:
+							if (!isPaused){
+								if (cotHienTai != 7) cotHienTai ++;
+							break;}
+						//phim P de tam dung choi game
+						case KeyEvent.VK_P:
+							isPaused=!isPaused;
+							
+						
+						}	
+					}
+					
+					
+				});
 				
-				
-				switch(e.getKeyCode()){
-				
-				//nut Q dung de quay hinh sang trai
-				case KeyEvent.VK_Q:
-					if (currentRotation==0) currentRotation=3;
-					else currentRotation--;
-					boardpanel.repaint();
-				break;
-				//phim E dung de quay hinh sang phai	
-				case KeyEvent.VK_E:
-					if (currentRotation==3) currentRotation=0;
-					else currentRotation++;
-					boardpanel.repaint();
-				break;
-				//phim A dung de dich trai
-				case KeyEvent.VK_A:
-					if (cotHienTai != 0) cotHienTai --;
-				break;
-				//phim D dung de dich phai
-				case KeyEvent.VK_D:
-					if (cotHienTai != 7) cotHienTai ++;
-				break;
-				//phim S dung de tang speed cho khoi gach
-				
-				}	
+				pack();
+				setVisible(true);
 			}
 			
+			public void CreateMenu(){
+				JMenuBar menubar = new JMenuBar();
+				JMenu menu;
+				JMenuItem menuitem;
+				
+				this.setJMenuBar(menubar);
+				menu=new JMenu("Game");
+				menu.setMnemonic(KeyEvent.VK_G);
+				menuitem = new JMenuItem("New Game");
+				menu.add(menuitem);
+				menuitem= new JMenuItem("Exit");
+				menuitem.setMnemonic(KeyEvent.VK_E);
+				menuitem.addActionListener(new MenuHandler(this));
+				menu.add(menuitem);
+				menubar.add(menu);
+			}
 			
-		});
-		
-		pack();
-		setVisible(true);
-	}
-	
-	public void CreateMenu(){
-		JMenuBar menubar = new JMenuBar();
-		JMenu menu;
-		JMenuItem menuitem;
-		
-		this.setJMenuBar(menubar);
-		menu=new JMenu("Game");
-		menu.setMnemonic(KeyEvent.VK_G);
-		menuitem = new JMenuItem("New Game");
-		menu.add(menuitem);
-		menuitem= new JMenuItem("Exit");
-		menuitem.setMnemonic(KeyEvent.VK_E);
-		menuitem.addActionListener(new MenuHandler(this));
-		menu.add(menuitem);
-		menubar.add(menu);
-	}
-	
-	public class MenuHandler implements ActionListener
-	   {
-		   Tetris ter;
-		   public MenuHandler(Tetris ter)
-		   {
-			   this.ter = ter;
-		   }
-		   public void actionPerformed(ActionEvent e) {
-			   String s = e.getActionCommand();
-			   if(s == "Exit")
+			public class MenuHandler implements ActionListener
 			   {
-				   ter.dispose();
+				   Tetris ter;
+				   public MenuHandler(Tetris ter)
+				   {
+					   this.ter = ter;
+				   }
+				   public void actionPerformed(ActionEvent e) {
+					   String s = e.getActionCommand();
+					   if(s == "Exit")
+					   {
+						   ter.dispose();
+					   }
+				   }
 			   }
-		   }
-	   }
-	
-	public void startGame(){
-		this.level=1;
-		this.score=0;
-		this.hangHienTai=2;
-		this.cotHienTai=3;
-		this.currentRotation=0;
-		
-			while(true){
-				sec=350000000;
-				while (sec>0) sec--;
-				updateGame();
 			
-		}
-		}
-		
-	
-	
+			public void startGame(){
+				this.level=1;
+				this.score=0;
+				this.hangHienTai=2;
+				this.cotHienTai=3;
+				this.currentRotation=0;
+				
+					while(true){
+						sec=350000000;
+						while (sec>0) sec--;
+						if(!isPaused)
+						updateGame();
+					
+				}
+				}
+				
+			
+			
 
-	public int getCurrentRotation() {
-		return currentRotation;
-	}
+			public int getCurrentRotation() {
+				return currentRotation;
+			}
 
-	public void updateGame(){
-		this.hangHienTai++;
-		boardpanel.repaint();
-	}
-	
-	public static void main(String[] args) {
-		Tetris tetris =new Tetris(); 
-		tetris.startGame();
-	}
+			public void updateGame(){
+				if(hangHienTai<18)
+				this.hangHienTai++;
+				boardpanel.repaint();
+			}
+			
+			public static void main(String[] args) {
+				Tetris tetris =new Tetris(); 
+				tetris.startGame();
+			}
 
-	public int getLevel() {
-		
-		return level;
-	}
+			public int getLevel() {
+				
+				return level;
+			}
 
-	public int getScore() {
-		return score;
-	}
+			public int getScore() {
+				return score;
+			}
 
-	public int getCotHienTai() {
-		return cotHienTai;
-	}
+			public int getCotHienTai() {
+				return cotHienTai;
+			}
 
-	public int getHangHienTai() {
-		return hangHienTai;
-	}
+			public int getHangHienTai() {
+				return hangHienTai;
+			}
 	
 }
